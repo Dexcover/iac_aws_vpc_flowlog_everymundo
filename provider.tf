5a7b3e798721b11f3e58bf58dd413f7aa4b6ffc7
Diego Chicaiza <diegochicaiza1@hotmail.com>
2021-12-08 17:50:39 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+    region = "us-east-1"
     assume_role {
         role_arn        = "arn:aws:iam::${var.account_id}:role/delegate-admin-em-provisioner"
         session_name    = "devops-terraform"
